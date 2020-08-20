6e101999b2787d87e964be364688be02ebba2bad
moonburnt <moonburnt@disroot.org>
2020-08-20 22:10:54 +0300

    Add files via upload

--- pykagapi.py
+++ pykagapi.py
@@ -18,10 +18,10 @@ class KAG_API:
 
     def get_game_info(self):
         '''Returns dictionary with general info about KAG'''
-        url = self.siteurl+"/game/thd/kag"
+        url = "{}/game/thd/kag".format(self.siteurl)
         data = requests.get(url, timeout = self.timeout)
         pydata = json.loads(data.text)
-        return pydata
+        return pydata['gameInfo']
 
     #Player shenanigans
     def get_player_overall_info(self, nickname):
@@ -36,21 +36,21 @@ class KAG_API:
         url = "{}/player/{}/info".format(self.siteurl, nickname)
         data = requests.get(url, timeout = self.timeout)
         pydata = json.loads(data.text)
-        return pydata
+        return pydata['playerInfo']
 
     def get_player_status(self, nickname):
         '''Receives player's nickname (str), returns dictionary with player's status'''
         url = "{}/player/{}/status".format(self.siteurl, nickname)
         data = requests.get(url, timeout = self.timeout)
         pydata = json.loads(data.text)
-        return pydata
+        return pydata['playerStatus']
 
     def get_player_banflags(self, nickname):
         '''Receives player's nickname (str), returns dictionary with player's banflags'''
         url = "{}/player/{}/banflags".format(self.siteurl, nickname)
         data = requests.get(url, timeout = self.timeout)
         pydata = json.loads(data.text)
-        return pydata
+        return pydata['playerBanFlags']
 
     def get_player_private_info(self, login, password, nickname=None):
         '''Receives your (unless you are admin) login, password, nickname (strs), returns dictionary with private info'''
@@ -60,7 +60,7 @@ class KAG_API:
         url = "{}/player/{}/myinfo".format(self.siteurl, nickname)
         data = requests.get(url, auth=(login, password), timeout = self.timeout)
         pydata = json.loads(data.text)
-        return pydata
+        return pydata['playerInfoPrivate']
 
     def get_player_private_forum_info(self, login, password, nickname=None):
         '''Receives your (unless you are admin) login, password, nickname (strs), returns dictionary with private forum info'''
@@ -69,7 +69,7 @@ class KAG_API:
         url = "{}/player/{}/foruminfo".format(self.siteurl, nickname)
         data = requests.get(url, auth=(login, password), timeout = self.timeout)
         pydata = json.loads(data.text)
-        return pydata
+        return pydata['playerInfoForum']
 
     def get_player_avatar(self, nickname, size="m"):
         '''Receives player's nickname and avatar size ("s", "m" or "l") (all strs), returns str link to pfp of that size'''
@@ -77,6 +77,13 @@ class KAG_API:
         data = requests.get(url, timeout = self.timeout)
         pydata = json.loads(data.text)
         return str(list(pydata.values())[0])
+
+    def get_player_avatars(self, nickname):
+        '''Receives str(nickname), returns dictionary with links to large, medium and small pfps'''
+        url = "{}/player/{}/avatar/".format(self.siteurl, nickname)
+        data = requests.get(url, timeout = self.timeout)
+        pydata = json.loads(data.text)
+        return pydata
 
     #Token shenanigans
     def get_token(self, login, password):
@@ -102,6 +109,49 @@ class KAG_API:
         data = requests.get(url, timeout = self.timeout)
         pydata = json.loads(data.text)
         return str(pydata['yourIP'])
+
+    def get_filters(self):
+        '''Receive dic(info) with valid filters used in server-related requests'''
+        url = "{}/game/1/servers/filterinfo".format(self.siteurl)
+        data = requests.get(url, timeout = self.timeout)
+        pydata = json.loads(data.text)
+        return pydata
+
+    def get_servers(self, filters=None):
+        '''Optionally receives list(filters). Returns list with matching servers (or all known servers, if used without filter). May take a while to process'''
+        if filters:
+            jfilters = json.dumps(filters)
+            url = self.siteurl+'/game/1/servers?filters='+jfilters
+        else:
+            url = "{}/game/1/servers".format(self.siteurl)
+        data = requests.get(url, timeout = self.timeout)
+        pydata = json.loads(data.text)
+        return pydata['serverList']
+
+    def get_alive_servers(self):
+        '''Returns list with servers thats up and running. Or at least according to api'''
+        filters = [{'field': 'current', 'op': 'eq', 'value': True}]
+        pydata = self.get_servers(filters)
+        return pydata
+
+    def get_active_servers(self):
+        '''Returns list with servers that contain players'''
+        filters = [{'field': 'current', 'op': 'eq', 'value': True}, {'field': 'currentPlayers', 'op': 'ge', 'value': 1}]
+        pydata = self.get_servers(filters)
+        return pydata
+
+    def get_server_status(self, ip, port):
+        '''Receives str(ip) and int(port), returns dictionary with server's info'''
+        url = "{}/game/thd/kag/server/{}/{}/status".format(self.siteurl, ip, port)
+        data = requests.get(url, timeout = self.timeout)
+        pydata = json.loads(data.text)
+        return pydata['serverStatus']
+
+    def get_server_minimap(self, ip, port):
+        '''Receives str(ip) and int(port), returns server's current map image in binary form (use something like Pillow+BytesIO to decode it)'''
+        url = "{}/game/thd/kag/server/{}/{}/minimap".format(self.siteurl, ip, port)
+        data = requests.get(url, timeout = self.timeout)
+        return data.content
 
 if __name__ == "__main__":
     print("This is the library, its not intended to be used directly.\nCheck project's github page for info about general usage:\nhttps://github.com/moonburnt/pykagapi/")
